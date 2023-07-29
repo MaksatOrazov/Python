745cc73c77bc7f18967dcfcd9b3281be732c26a5
MaksatOrazov <maxi.orazov1996@gmail.com>
2023-07-29 22:51:04 +0400

    Day_1

---
+++ Self-Development/day_1.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 28 22:10:21 2023
+
+@author: pc
+"""
+
+#%%
+print("Hello World")
+print("1 st number = ",5)
+print(3*8)
+print(4+6, (15-6)*4)
+print(17/5)
+print(17//5)
+print(17%6)
+print(4**4)
+
+#%%
+
+#%% Question - 1
+a = int(input("Number a = "))
+c = int(input("Number b = "))
+print(a*c/2)
+#%%
+
+#%% Question - 2
+
+apple = int(input("How many apples? "))
+children = int(input("How many children? "))
+
+print("Apple for each child = " + str(apple//children) + 
+      """\nNumber of apples remaining = """ + str(apple%children))
+#%% Question - 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
